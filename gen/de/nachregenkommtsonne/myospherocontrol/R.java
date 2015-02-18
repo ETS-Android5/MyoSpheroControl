67ea26cd0d3dfdbb05ec9c9c289db475ba203899
Bernd <Bernd@533b6b32-d271-4e35-8edc-18ab391a8b2f>
2015-02-18 05:33:20 +0000

    manual for control mode new launcher icon
    git-svn-id: svn+ssh://blue.3dallee.de/bernd/Java/MyoSpheroControl@308 533b6b32-d271-4e35-8edc-18ab391a8b2f

--- gen/de/nachregenkommtsonne/myospherocontrol/R.java
+++ gen/de/nachregenkommtsonne/myospherocontrol/R.java
@@ -45,21 +45,22 @@ public final class R {
         public static final int spread_fingers=0x7f020008;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000d;
+        public static final int action_settings=0x7f09000e;
         public static final int container=0x7f090000;
-        public static final int hintText=0x7f090007;
-        public static final int myoConnectedIcon=0x7f090003;
-        public static final int myoLinkedIcon=0x7f090002;
-        public static final int myoSyncronizedIcon=0x7f090004;
-        public static final int myosdk__action_scan=0x7f09000e;
-        public static final int myosdk__connection_state_dot=0x7f09000b;
-        public static final int myosdk__fragment_scan=0x7f090008;
-        public static final int myosdk__progress=0x7f09000a;
-        public static final int myosdk__required_firmware_version_text=0x7f090009;
-        public static final int myosdk__scan_result_view=0x7f09000c;
-        public static final int spheroConnectedIcon=0x7f090006;
-        public static final int spheroDiscoveredIcon=0x7f090005;
+        public static final int hintText=0x7f090008;
+        public static final int myoConnectedIcon=0x7f090004;
+        public static final int myoLinkedIcon=0x7f090003;
+        public static final int myoSyncronizedIcon=0x7f090005;
+        public static final int myosdk__action_scan=0x7f09000f;
+        public static final int myosdk__connection_state_dot=0x7f09000c;
+        public static final int myosdk__fragment_scan=0x7f090009;
+        public static final int myosdk__progress=0x7f09000b;
+        public static final int myosdk__required_firmware_version_text=0x7f09000a;
+        public static final int myosdk__scan_result_view=0x7f09000d;
+        public static final int spheroConnectedIcon=0x7f090007;
+        public static final int spheroDiscoveredIcon=0x7f090006;
         public static final int startButton=0x7f090001;
+        public static final int textView1=0x7f090002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -75,6 +76,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f060008;
         public static final int app_name=0x7f060006;
+        public static final int controlUsage=0x7f06000b;
         public static final int myosdk__action_scan=0x7f060001;
         public static final int myosdk__action_stop_scan=0x7f060002;
         public static final int myosdk__firmware_required_format=0x7f060005;
