ac6eadde1d3968e7a86ed28110b2434f3a5bb01b
Bernd Herzog <bernd.herzog@nachregenkommtsonne.de>
2016-04-30 00:19:24 +0200

    refactorings

--- MyoSpheroControl/src/de/nachregenkommtsonne/myospherocontrol/controller/notification/NotificationUpdater.java
+++ MyoSpheroControl/src/de/nachregenkommtsonne/myospherocontrol/controller/notification/NotificationUpdater.java
@@ -5,8 +5,6 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import de.nachregenkommtsonne.myospherocontrol.GuiStateHinter;
 import de.nachregenkommtsonne.myospherocontrol.R;
 import de.nachregenkommtsonne.myospherocontrol.activity.ControlActivity;
